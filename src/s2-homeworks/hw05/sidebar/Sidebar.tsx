a2b92de11271421b28906f8865b4c10136e7dffb
anna-7nova <anna.7nova@yandex.by>
2025-05-21 19:40:06 +0300

    feat(HW5): add styles in molule.css for Header and Sidebar

---
+++ src/s2-homeworks/hw05/sidebar/Sidebar.tsx
@@ -0,0 +1,62 @@
+import React, {FC} from 'react'
+import {NavLink, useLocation} from 'react-router-dom'
+import s from './Sidebar.module.css'
+import {PATH} from '../Pages'
+import closeIcon from './closeOutline.svg'
+
+type PropsType = {
+    open: boolean
+    handleClose: () => void
+}
+
+export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
+const location =  useLocation()
+const currentPath = location.pathname
+
+    const sidebarClass = s.sidebar
+        + (open ? ' ' + s.open : '')
+        const navIsActive = s.a + " " + s.active
+    return (
+        <>
+            {/*затемнение справа от открытого меню*/}
+            {open && <div className={s.background} onClick={handleClose}/>}
+
+            <aside className={sidebarClass}>
+                <button className={s.close} onClick={handleClose}>
+                    <img
+                        src={closeIcon}
+                        alt="close sidebar"
+                        id={'hw5-menu-close'}
+                    />
+                </button>
+
+                <nav id={'hw5-menu'} className={s.nav}>
+                    <NavLink
+                        id={'hw5-pre-junior-link'}
+                        to={PATH.PRE_JUNIOR}
+                        onClick={handleClose}
+                        className={currentPath===PATH.PRE_JUNIOR? navIsActive: s.a } 
+                    >
+                        Pre-junior
+                    </NavLink>
+                    <NavLink
+                        id={'hw5-junior-link'}
+                        to={PATH.JUNIOR}
+                        onClick={handleClose}
+                        className={currentPath===PATH.JUNIOR? navIsActive: s.a }  
+                    >
+                        Junior
+                    </NavLink>
+                    <NavLink
+                        id={'hw5-junior-plus-link'}
+                        to={PATH.JUNIOR_PLUS}
+                        onClick={handleClose}
+                        className={currentPath===PATH.JUNIOR_PLUS? navIsActive: s.a } 
+                    >
+                        Junior Plus
+                    </NavLink>
+                </nav>
+            </aside>
+        </>
+    )
+}
