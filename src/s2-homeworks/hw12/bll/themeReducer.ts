1882dd0928dcdcce307eaa3bc6c997812270fad2
annaSemenova <anna.7nova@yandex.by>
2025-08-29 12:42:54 +0300

    feat(hw12): add theme switcher

---
+++ src/s2-homeworks/hw12/bll/themeReducer.ts
@@ -0,0 +1,14 @@
+const initState = {
+    themeId: 1,
+}
+
+export const themeReducer = (state = initState, action: {type: string, id: number}): {themeId: number} => { 
+    switch (action.type) {
+        case 'SET_THEME_ID': 
+        return {...state, themeId: action.id}
+        default:
+            return state
+    }
+}
+
+export const changeThemeId = (id: number): {type: string, id: number} => ({ type: 'SET_THEME_ID', id })
